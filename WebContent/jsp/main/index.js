c71124460a46afa35d6534fe268736215709ef9f
ctf0106 <ctf0106@163.com>
2018-06-07 01:39:35 +0800

    主页修订

--- WebContent/jsp/main/index.js
+++ WebContent/jsp/main/index.js
@@ -11,22 +11,15 @@ $.ajax({
     		 var leaf=result[i].leaf;
     		 var text=result[i].text;
     		 var url=result[i].url;
-    		 alert(url);
     		 var html="";
+    		 html+="<div class='layui-row row-space'><div class='layui-col-xs12'><div class='layui-card'><div class='layui-card-header'>"+text+"</div>";
+    		 html+="<div class='layui-card-body layadmin-takerates'>";
+			 html+="<ul class='layui-row layui-col-space10 layui-this'>";
     		 for(var j=0;j<currentDataArray.length;j++){
-    			 html+"<div class='layui-row row-space'><div class='layui-col-xs12'><div class='layui-card'><div class='layui-card-header'>药品进货管理</div>";
-             	html+"<div class='layui-card-body layadmin-takerates'>";
-             	html+"<ul class='layui-row layui-col-space10 layui-this'>";
-             	html+"<li class='layui-col-xs2'><a lay-href='home/homepage1'><i class='layui-icon layui-icon-console'></i><cite>进货入库</cite></a></li>";
-             	html+"<li class='layui-col-xs2'><a lay-href='home/homepage1'><i class='layui-icon layui-icon-console'></i><cite>退货出库</cite></a></li>";
-             	html+"<li class='layui-col-xs2'><a lay-href='home/homepage1'><i class='layui-icon layui-icon-search'></i><cite>进货单据查询</cite></a></li>";
-             	html+"<li class='layui-col-xs2'><a lay-href='home/homepage1'><i class='layui-icon layui-icon-search'></i><cite>退货单据查询</cite></a></li>";
-             	html+"<li class='layui-col-xs2'><a lay-href='home/homepage1'><i class='layui-icon layui-icon-search'></i><cite>当前库存查询</cite></a></li>";
-             	html+"</ul>";
-             	html+"</div></div></div></div>";		 
+    			 html+="<li class='layui-col-xs2'><a href='"+currentDataArray[j].url+"'><i class='layui-icon layui-icon-search'></i><cite>"+currentDataArray[j].text+"</cite></a></li>";
     		 }
+    		 html+="</ul></div></div></div></div>"
     		$("#mainContainer").append(html);
-    		console.log(html)
     	 }
     	 	
  	 }
